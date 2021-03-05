fc3b09518c9bf2f58a51feab2ba22a72665b492a
TSUYURI <tsuyuri@grit-group.co.jp>
2021-03-05 17:02:22 +0900

    remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require("path");
 var glob = require("glob");
 var constants_1 = require("ts-loader/dist/constants");
 var entries = glob.sync("./src/modules/*.ts"); // get all js file in modules directory
-// webpack rules
+
 var rules = [
     {
         test: /\.ts$/,
